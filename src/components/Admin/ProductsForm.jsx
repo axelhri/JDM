bf4f1672655622347d0ed26a762aba5516a5331a
axelhri <166370210+axelhri@users.noreply.github.com>
2024-11-25 23:09:46 +0100

    Update ProductsForm.jsx

--- src/components/Admin/ProductsForm.jsx
+++ src/components/Admin/ProductsForm.jsx
@@ -29,7 +29,7 @@ function ProductsForm() {
       }
 
       const response = await axios.post(
-        "http://localhost:5000/api/v1/products",
+        "https://jdm-back-end-kvxh.onrender.com/api/v1/products",
         products,
         {
           headers: {
